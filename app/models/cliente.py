1286e2e13454d64a55cc1ba969a107d2d53d6e45
Matheus Batistussi <batistussi.m@gmail.com>
2017-06-19 14:06:22 -0300

    Substituição do endereço pelas coordenadas.

--- app/models/cliente.py
+++ app/models/cliente.py
@@ -2,8 +2,8 @@ from pessoa import *
 
 class Cliente(Pessoa):
 
-	def __init__(self, nome, cpf = '', endereco, lixeira = None):
-		Pessoa.__init__(nome, cpf, endereco)
+	def __init__(self, nome, cpf = '', coord_x, coord_y, lixeira = None):
+		Pessoa.__init__(nome, cpf, coord_x, coord_y)
 		self.__lixeira = lixeira
 
 	def get_lixeira(self):
--- app/models/pessoa.py
+++ app/models/pessoa.py
@@ -2,7 +2,8 @@
 
 class Pessoa(object):
 
-	def __init__(self, nome, cpf, endereco):
+	def __init__(self, nome, cpf, coord_x, coord_y):
 		self.__nome = nome
 		self.__cpf = cpf
-		self.__endereco = endereco
+		self.__coord_x = coord_x
+		self.__coord_y = coord_y
