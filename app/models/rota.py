f0c6c9e071e9e11a32590bd41edc9a523257d6fa
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-06-19 23:01:35 -0300

    Adição de métodos na classe de rota

--- app/models/rota.py
+++ app/models/rota.py
@@ -1,4 +1,6 @@
 from bairro import *
+from cliente import *
+import random
 
 class Rota(object):
 
@@ -22,9 +24,31 @@ class Rota(object):
 		pass
 		# Pegar lixeiras do bairro correspondente ao bairro que se quer gerar a rota
 
-	def __gerar_rota_padrao(self):
-		pass
-		# Gerar rota com base apenas na qunatidade de lixo por lixeira ou cliente que realizou solicitação
-
 	def __gerar_rota_personalizada(self):
 		pass
+
+	# Gerar rota com base apenas na qunatidade de lixo por lixeira ou cliente que realizou solicitação
+	def __gerar_rota_padrao(self):
+		while self.__list_coleta.length > 0:
+			atual = self.__calcula_proximo(0, 0, 0)
+			lista_atual.append(atual)
+			for c in clientlist:
+				if c.id == atual:
+					clientlist.remove(c)
+
+	def __calcula_proximo(self, x_atual, y_atual, primeiro):
+		mais_proximo_distancia = 100
+		for c in self.__list_coleta:
+			distancia = math.sqrt( ((0 - c.coord_x)**2) + ((0 - c.coord_y)**2))
+
+			if primeiro == 0:
+				primeiro = 1
+				mais_proximo = c.id
+				mais_proximo_distancia = distancia
+				continue
+
+			if distancia < mais_proximo_distancia:
+				mais_proximo_distancia = distancia
+				mais_proximo = c.id
+
+		return mais_proximo
